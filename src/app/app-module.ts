1fb4c7313b49875ddfe8b1c1cb2730eb0a270a1b
PabloPcCap <pablo.a.cruz@capgemini.com>
2025-06-09 10:30:39 -0300

    Commit First

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,18 +1,13 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { App } from './app';
+import { App } from './app.component';
+import { Header } from './header/header.component';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App, Header],
+  imports: [BrowserModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'component-header',
+  standalone: false,
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class Header {}
